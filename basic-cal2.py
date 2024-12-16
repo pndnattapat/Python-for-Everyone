b4f8165f4525d0e1eabb973434538c51d1fb03fb
pndnattapat <pndnattapat@gmail.com>
2024-12-16 14:34:04 +0700

    Update สีกระเบื้อง

--- basic-cal2.py
+++ basic-cal2.py
@@ -1,5 +1,10 @@
-tilecolor = {'white':90,'red':100,'gold':200}
-print('------- โปรแกรมคำนวณกระเบื้อง -------')
+tilecolor = {'white':90,'red':100,'gold':200,'grey':120}
+
+print('------- ราคากระเบื้อง -------')
+for c,t in tilecolor.items():
+    print(f'สี: {c} ราคา: {t}')
+
+print('------- โปรแกรมคำนวณกระเบื้อง V.2 -------')
 try:
     tiles = int(input ('คุณต้องการปูกระเบื้องทั้งหมดกี่แผ่น : '))
     row = int(input ('1 แถวสามารถปูกระเบื้องได้กี่แผ่น : '))
@@ -8,13 +13,12 @@ except:
     print('กรุณากรอกข้อมูลเป็นตัวเลขเท่านั้น!')
     tiles = int(input ('คุณต้องการปูกระเบื้องทั้งหมดกี่แผ่น : '))
     row = int(input ('1 แถวสามารถปูกระเบื้องได้กี่แผ่น : '))
-    color = input('กรุณาเลือกสีกระเบื้อง [white/red/gold] : ')
+    color = input('กรุณาเลือกสีกระเบื้อง [white/red/gold/grey] : ')
     
 print('------- Calculate -------')
 total_row = tiles // row
 remain_tiles =  tiles % row
 
-# print(total_row,remain_tiles)
 
 buy_more = row - remain_tiles
 
@@ -25,3 +29,5 @@ print('ต้องปูกระเบื้องทั้งหมด {} แ
 print('เหลือกระเบื้องที่ต้องปู {} แผ่น'.format(remain_tiles))
 print('ลูกค้าต้องซื้อกระเบื้องเพิ่ม {} แผ่น'.format(buy_more))
 print(f'จำนวนเงินที่ต้องซื้อกระเบื้องเพิ่ม {buy_more * tilecolor[color]} บาท')
+
+print('---------- The End -----------')
